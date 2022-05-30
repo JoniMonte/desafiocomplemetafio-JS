07f474a14833e42e4562d95a1194f436ded682fd
Jonatan <rolfi_jo23@outlook.com>
2022-05-30 14:46:08 -0300

    agregando archivos

---
+++ main.js
@@ -0,0 +1,8 @@
+
+let ingreso = prompt("ingresa una PALABRA \n para salir ingresa ESC");
+while (ingreso != "ESC") {
+    //ejecuta el codigo imprimiendo en consola
+    console.log("El producto ingresado es "+ ingreso);
+    //condicion de salida
+    ingreso = prompt("ingresa una PALABRA \n para salir ingresa ESC");
+}
